2f33a5ec88e89b5454e3c6a5173cb32cf8b49aed
Gabriel Gutierrez Quispe <gabriel.gutierrezq@vallegrande.edu.pe>
2022-12-14 16:54:37 +0000

    f

--- static/js/botonesOE.js
+++
@@ -1,9 +0,0 @@
-function displayUno(id){
-    document.getElementById('sec1').style.display = 'block';
-    document.getElementById('sec2').style.display = 'none';
-}
-
-function displayDos(id){
-    document.getElementById('sec1').style.display = 'none';
-    document.getElementById('sec2').style.display = 'block';
-}
