5beb5f2a185be9b6e5d25b40890f2465e4702e15
Gabriel Gutierrez Quispe <gabriel.gutierrezq@vallegrande.edu.pe>
2022-12-15 18:36:16 +0000

    v

--- conexionDB/registro.js
+++ conexionDB/registro.js
@@ -1,4 +1,4 @@
-const url = "http://3.216.207.35:3000/api/pedido/";
+const url = "http://34.194.199.232:3000/api/pedido/";
 let resultados = '';
 const formArticulo = document.querySelector("form");
 const userped = document.getElementById("USERPED");
